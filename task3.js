5441a7cea2c49faf8c79537fa01478b6bea897de
Bohdana <bogdana.vaschchuk@gmail.com>
2024-03-16 22:43:04 +0200

    Commit the fixed issues with tests

--- task3.js
+++ task3.js
@@ -4,7 +4,7 @@ function ternar(a) {
     if (typeof a === 'number' ){
         let b = 2;
         let c = a % b;
-        let count = c === 0 ? 'You number is an integer' : 'You number is not an integer'
+        let count = c === 0 ? 'Your number is an integer' : 'Your number is not an integer'
         console.log(count)
     } else {
         console.log('Your value incorrect, we work only with number')
